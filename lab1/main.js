06bbaebac89d39c96cbf3435803ef610b76407ee
Даник <pistriuga.danylo@lll.kpi.ua>
2025-03-13 23:52:12 +0200

    add interactive mode

--- lab1/main.js
+++ lab1/main.js
@@ -1,15 +1,6 @@
 const fs = require("fs");
 const readline = require("readline");
 
-function validCheck(...arr) {
-  const invalid = arr.find((el) => !isFinite(el));
-
-  if (invalid)
-    return `Error. Expected a valid real number, got ${invalid} instead`;
-
-  return true;
-}
-
 function equation(a, b, c) {
   if (a === 0) return { success: false, message: "argument a cannot be 0" };
 
@@ -32,6 +23,13 @@ function equation(a, b, c) {
   return { success: true, roots: [x1, x2] };
 }
 
+function getAnswer(a, b, c) {
+  const res = equation(a, b, c);
+  if (!res.success) console.log(res.message);
+  else
+    res.roots.forEach((root, index) => console.log(`x${index + 1}: ${root}`));
+}
+
 if (process.argv.length === 3) {
   const filePath = process.argv[2];
 
@@ -46,11 +44,54 @@ if (process.argv.length === 3) {
       console.log("wrong file format. expected: a b c\\n");
       return;
     }
+
     const [a, b, c] = args.map(Number);
 
-    const res = equation(a, b, c);
-    if (!res.success) console.log(res.message);
-    else
-      res.roots.forEach((root, index) => console.log(`x${index + 1}: ${root}`));
+    let isValid = true;
+    [a, b, c].forEach((el) => {
+      if (!isFinite(Number(el))) {
+        isValid = false;
+        console.log(`Error. Expected a valid real number, got ${el} instead`);
+      }
+    });
+    if (!isValid) return;
+
+    getAnswer(a, b, c);
   });
+}
+
+function askQuetion(query) {
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(query, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
+
+async function getValidNumber(promptText) {
+  while (true) {
+    const input = await askQuetion(promptText);
+    const num = Number(input);
+    if (isFinite(num)) return num;
+
+    console.log(`Error. Expected a valid real number, got '${num}' instead`);
+  }
+}
+
+async function interactiveMode() {
+  const a = await getValidNumber("a: ");
+  const b = await getValidNumber("b: ");
+  const c = await getValidNumber("c: ");
+
+  getAnswer(a, b, c);
+}
+
+if (process.argv.length === 2) {
+  interactiveMode();
 }
